94cc818641cde7f1df573306c2e5e5786a712e4b
sid55 <sidhant.plb@gmail.com>
2018-01-11 00:39:51 +0530

    Initial Commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn pritam.wsgi --log-file -
---
+++ website/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from rest_framework import status
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
